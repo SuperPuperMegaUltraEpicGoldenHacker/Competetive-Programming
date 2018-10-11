61e483d39630e81562f384ffc4ab0ecb0976d6e0
Zhanbolat <kussianov.zhanbolat@gmail.com>
2018-10-11 19:10:41 +0600

    codeing

--- Algo/List/code.cpp
+++ Algo/List/code.cpp
@@ -1,108 +1,137 @@
 #include<iostream>
 using namespace std;
-class Node{
- public:
-  int val;
-  Node *next, *prev;
-  Node(int asd){
-  val = asd;
-  next = NULL;
-  prev = NULL;
- }
+class Node {
+    public:
+        int val;
+        Node *next, *prev;
+    Node(int asd){
+        val = asd;
+        next = NULL;
+        prev = NULL;
+    }
 };
 class LinkedList{
- public:
-  Node *head, *tail;
- LinkedList(){
-  head = NULL;
-  tail = NULL;
- }
- void insertFirst(int x){
-  Node *node = new Node(x);
-  if(head ==NULL){
-   head = node;
-   tail = node;
-   return;
-  }else{
-   head -> prev = node;
-   node -> next = head;
-   head = node;
-  }
- }
- void deleteFirst(){
-  head = head -> next;
-  if( head != NULL){
-   head -> prev = NULL;
-  }else{
-   tail = NULL;
-  }
- }
- void deleteLast(){
-  tail = tail -> prev;
-  if( tail != NULL){
-   tail -> next = NULL;
-  }else{
-   head = NULL;
-  }
- }
- void del(int x){
-  Node *cur = head;
-  while(cur && cur -> val != x){
-   cur = cur -> next;
-  }
-  if(!cur){
-   return;
-  }
-  if( cur == head){
-   deleteFirst();
-  }else if( cur == tail){
-   deleteLast();
-  }else{
-   cur -> next -> prev = cur -> prev;
-   cur -> prev -> next = cur -> next;
-  }
- }
- void print(){
-  Node *cur = head;
-  while(cur){
-   if( cur != head){
-    cout<<" ";
-   }
-   cout<<(cur -> val);
-   cur = cur -> next;
-  }
-  cout<<endl;
- }
+    public:
+        Node *head, *tail;
+    LinkedList(){
+        head = NULL;
+        tail = NULL;
+    }
+    int cnt (int x) {
+      int res = 0;
+      for (Node *i = head; i; i = i->next) {
+        if (i->val == x)
+          res++;
+      }
+      return res;
+    }
+    int getNth (int n) {
+      int id = 0;
+      for (Node *i = head; i; i = i->next) {
+        if (id == n)
+          return i->val;
+        id++;
+      }
+      return -1;
+    }
+    void insertLast (int x) {
+      Node *node = new Node(x);
+      if(tail == NULL){
+          head = node;
+          tail = node;
+          return;
+      }else{
+          tail -> next = node;
+          node -> prev = tail;
+          tail = node;
+      }
+    }
+    void insertFirst(int x){
+        Node *node = new Node(x);
+        if(head ==NULL){
+            head = node;
+            tail = node;
+            return;
+        }else{
+            head -> prev = node;
+            node -> next = head;
+            head = node;
+        }
+    }
+    void deleteFirst(){
+        head = head -> next;
+        if( head != NULL){
+            head -> prev = NULL;
+        }else{
+            tail = NULL;
+        }
+    }
+    void deleteLast(){
+        tail = tail -> prev;
+        if( tail != NULL){
+            tail -> next = NULL;
+        }else{
+            head = NULL;
+        }
+    }
+    void del(int x){
+        Node *cur = head;
+        while(cur && cur -> val != x){
+            cur = cur -> next;
+        }
+        if(!cur){
+            return;
+        }
+        if( cur == head){
+            deleteFirst();
+        }else if( cur == tail){
+            deleteLast();
+        }else{
+            cur -> next -> prev = cur -> prev;
+            cur -> prev -> next = cur -> next;
+        }
+    }
+    void print(){
+        Node *cur = head;
+        while(cur){
+            if( cur != head){
+                cout<<" ";
+            }
+            cout<<(cur -> val);
+            cur = cur -> next;
+        }
+        cout<<"\n";
+    }
 };
 int main(){
- int n,x;
- string s;
- LinkedList *linkedList = new LinkedList();
- cin>>n;
- for(int i=1 ; i<=n ; i++){
-  cin>>s;
-  if(s=="insertFirst"){
-   cin>>x;
-   linkedList -> insertFirst(x);
-  } if( s== "delete"){
-   cin>>x;
-   linkedList -> del(x);
-  } if(s == "deleteFirst"){
-   linkedList -> deleteFirst();
-  } if(s == "deleteLast"){
-   linkedList -> deleteLast();
-  } if(s == "insertLast"){
-   cin>>x ;
-   linkedList -> insertLast(x);
-  } if(s == "cnt"){
-   cin>>x;
-   cout<<linkedList -> cnt(x) << endl;
+    int n,x;
+    string s;
+    LinkedList *linkedList = new LinkedList();
+    cin>>n;
+    for(int i=1 ; i<=n ; i++){
+        cin>>s;
+        if(s=="insertFirst"){
+            cin>>x;
+            linkedList -> insertFirst(x);
+        } if( s== "delete"){
+            cin>>x;
+            linkedList -> del(x);
+        } if(s == "deleteFirst"){
+            linkedList -> deleteFirst();
+        } if(s == "deleteLast"){
+            linkedList -> deleteLast();
+        } if(s == "insertLast"){
+            cin>>x ;
+            linkedList -> insertLast(x);
+        } if(s == "cnt"){
+            cin>>x;
+            cout<<linkedList -> cnt(x) << endl;
 
-  } if(s == "getNth"){
-   cin>>x;
-   cout<<linkedList -> getNth(x)<< endl;
-  }
- }
- linkedList -> print();
- return 0;
+        } if(s == "getNth"){
+            cin>>x;
+            cout<<linkedList -> getNth(x)<< endl;
+        }
+    }
+    linkedList -> print();
+    return 0;
 }
