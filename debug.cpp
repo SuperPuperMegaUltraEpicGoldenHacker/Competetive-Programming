49313c05aeabb65a5e62cfcdf98d49411ca76550
SuperPuperMegaUltraEpicGoldenHacker <33758315+SuperPuperMegaUltraEpicGoldenHacker@users.noreply.github.com>
2019-11-28 00:10:48 +0600

    debug

---
+++ debug.cpp
@@ -0,0 +1,164 @@
+# include <bits/stdc++.h>
+
+# define x first
+# define y second
+# define mp make_pair
+// everything goes according to my plan
+# define pb push_back
+# define sz(a) (int)(a.size())
+# define vec vector
+// shimkenttin kyzdary, dzyn, dzyn, dzyn...
+# define y1    Y_U_NO_y1
+# define left  Y_U_NO_left
+# define right Y_U_NO_right
+
+
+using namespace std;
+
+
+typedef pair <int, int> pii;
+typedef long long ll;
+typedef long double ld;
+
+const int Mod = (int)1e9 + 7;
+const int MX = 1073741822;
+const ll MXLL = 4e18;
+const int Sz = 1110111;
+// a pinch of soul
+inline void Read_rap () {
+  ios_base :: sync_with_stdio(0);
+  cin.tie(0); cout.tie(0);
+}
+inline void randomizer3000 () {
+  unsigned int seed;
+  asm ("rdtsc" : "=A"(seed));
+  srand (seed);
+}
+void files (string problem) {
+  if (fopen ((problem + ".in").c_str(),"r")) {
+    freopen ((problem + ".in").c_str(),"r",stdin);
+    freopen ((problem + ".out").c_str(),"w",stdout);
+  }
+}
+void localInput(const char in[] = "s") {
+  if (fopen (in, "r")) {
+    freopen (in, "r", stdin);
+  }
+  else
+    cerr << "Warning: Input file not found" << endl;
+}
+
+// Copied from G.Korotkevitch's template 
+template <typename A, typename B>
+string to_string(pair<A, B> p);
+ 
+template <typename A, typename B, typename C>
+string to_string(tuple<A, B, C> p);
+ 
+template <typename A, typename B, typename C, typename D>
+string to_string(tuple<A, B, C, D> p);
+ 
+string to_string(const string& s) {
+	return '"' + s + '"';
+}
+ 
+string to_string(const char* s) {
+	return to_string((string) s);
+}
+ 
+string to_string(bool b) {
+	return (b ? "true" : "false");
+}
+ 
+string to_string(vector<bool> v) {
+	bool first = true;
+	string res = "{";
+	for (int i = 0; i < static_cast<int>(v.size()); i++) {
+		if (!first) {
+			res += ", ";
+		}
+		first = false;
+		res += to_string(v[i]);
+	}
+	res += "}";
+	return res;
+}
+ 
+template <size_t N>
+string to_string(bitset<N> v) {
+	string res = "";
+	for (size_t i = 0; i < N; i++) {
+		res += static_cast<char>('0' + v[i]);
+	}
+	return res;
+}
+ 
+template <typename A>
+string to_string(A v) {
+	bool first = true;
+	string res = "{";
+	for (const auto &x : v) {
+		if (!first) {
+			res += ", ";
+		}
+		first = false;
+		res += to_string(x);
+	}
+	res += "}";
+	return res;
+}
+ 
+template <typename A, typename B>
+string to_string(pair<A, B> p) {
+	return "(" + to_string(p.first) + ", " + to_string(p.second) + ")";
+}
+ 
+template <typename A, typename B, typename C>
+string to_string(tuple<A, B, C> p) {
+	return "(" + to_string(get<0>(p)) + ", " + to_string(get<1>(p)) + ", " + to_string(get<2>(p)) + ")";
+}
+ 
+template <typename A, typename B, typename C, typename D>
+string to_string(tuple<A, B, C, D> p) {
+	return "(" + to_string(get<0>(p)) + ", " + to_string(get<1>(p)) + ", " + to_string(get<2>(p)) + ", " + to_string(get<3>(p)) + ")";
+}
+ 
+template <typename A, typename B, typename C, typename D, typename E>
+string to_string(tuple<A, B, C, D, E> p) {
+	return "(" + to_string(get<0>(p)) + ", " + to_string(get<1>(p)) + ", " + to_string(get<2>(p)) + ", " + to_string(get<3>(p)) + "," + to_string(get<4>(p)) + ")";
+}
+ 
+void debug_out() {
+	cerr << endl;
+}
+ 
+template <typename Head, typename... Tail>
+void debug_out(Head H, Tail... T) {
+	cerr << " " << to_string(H);
+	debug_out(T...);
+}
+ 
+#ifdef Local
+#define debug(...) cerr << "[" << #__VA_ARGS__ << "]:", debug_out(__VA_ARGS__)
+#else
+#define debug(...) 42
+#endif
+
+int main()
+{
+  # ifdef Local
+    //localInput();
+  # endif
+  Read_rap();
+  int x;
+  debug(x); 
+
+  return 0;
+}
+
+
+
+
+
+
+// Coded by Z..
